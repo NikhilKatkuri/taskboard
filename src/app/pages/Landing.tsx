f7286979aab58c6685069ed895f6da8de9c7e8f4
NikhilKatkuri <knikhil07k@gmail.com>
2025-12-10 08:41:30 +0530

    Chore : started dev UI

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -1,5 +1,42 @@
+import { useNavigate } from "react-router-dom";
+
 const Landing = () => {
-  return <div className="h-screen w-screen ">Hello, World!</div>;
+  const nav = useNavigate();
+  return (
+    <>
+      <div className="h-screen w-screen relative bg-gray-50">
+        <header className="h-16 w-screen bg-white/60 z-10 sticky top-0 flex  justify-between items-center px-2 shadow-xs">
+          <div className="flex">
+            <img src="/logo-1.svg" />
+          </div>
+          <div className=""></div>
+          <div className="flex items-center gap-1 ">
+            <button
+              onClick={() =>
+                setTimeout(() => {
+                  nav("/login");
+                }, 300)
+              }
+              className="h-8 rounded-md px-4 text-sm bg-[var(--theme-color)]/10 text-[var(--theme-color)] cursor-pointer transition-all ease-in-out duration-200 active:scale-90"
+            >
+              Login
+            </button>
+            <button
+              onClick={() =>
+                setTimeout(() => {
+                  nav("/register");
+                }, 300)
+              }
+              className="h-8 rounded-md px-4 text-sm bg-[var(--theme-color)] text-white cursor-pointer transition-all ease-in-out duration-200  active:scale-90"
+            >
+              Signup
+            </button>
+          </div>
+        </header>
+        <main className=""></main>
+      </div>
+    </>
+  );
 };
 
 export default Landing;
--- src/app/pages/auth/Login.tsx
+++ src/app/pages/auth/Login.tsx
@@ -1,5 +1,46 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center flex-row lg:p-4 p-2">
+        <main className="flex-1 w-full h-full flex flex-col justify-betweenr gap-4">
+          <div className="flex mt-3">
+            <img src="/logo-1.svg" />
+          </div>
+          <div className="mx-auto max-w-80  w-full h-64">
+            <div className="text-center">
+              <h1 className="text-3xl font-bold mb-2 ">Welcome Back</h1>
+              <p className="text-gray-600 mb-6">
+                Please login to your account to continue.
+              </p>
+            </div>
+            <div className="mt-4">
+              <form className="space-y-4">
+                <div className="">
+                  <input
+                    type="text"
+                    placeholder="username or email address"
+                    className="w-full h-12 rounded-xl border outline-0 px-3 border-gray-400 focus:border-gray-700 transition focus:shadow-sm "
+                  />
+                </div>
+                <div className="">
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="w-full h-12 rounded-xl border outline-0 px-3 border-gray-400 focus:border-gray-700 transition focus:shadow-sm "
+                  />
+                </div>
+                <button className="h-12 w-full bg-[var(--theme-color)] rounded-xl text-white font-medium ">
+                  login
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className=""></div>
+        </main>
+        <aside className="lg:block w-96 bg-black h-full hidden rounded-md"></aside>
+      </div>
+    </>
+  );
 };
 
 export default Login;
